925a32cbb769ccd91d5065f7d6bd2eb569cc5155
KenanYang <y-kenan@live.com>
2019-02-10 15:08:51 +1100

    Java self-defined exceptions

---
+++ Exceptions/src/day8/RideHorseException.java
@@ -0,0 +1,7 @@
+package day8;
+
+public class RideHorseException extends Exception {
+	public RideHorseException(int health) {
+		super("当前健康值为" + health + "，骑马失败！");
+	}
+}
--- Exceptions/src/day8/ThrowDemo.java
+++ Exceptions/src/day8/ThrowDemo.java
@@ -8,7 +8,9 @@ public class ThrowDemo {
 	public static void main(String[] args) {
 		Hero hero = new Hero("貂蝉");
 		try {
-			hero.setExp(-1000);
+			hero.setExp(1000);
+			hero.setHealth(40);
+			hero.rideHorse();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -21,9 +23,32 @@ public class ThrowDemo {
 class Hero{
 	private String name;
 	private long exp;
+	private int health; 
+	
+	
 	public Hero(String name) {
 		setName(name);
 	}
+	/**
+	 * 英雄骑马方法 - 当英雄的健康值低于50时，不允许骑马
+	 * @throws LessThanZeroException 
+	 */
+	public void rideHorse() throws RideHorseException {
+		if(getHealth() >= 50)
+			System.out.println(getName() + "正在快乐的骑马！");
+		else
+			throw new RideHorseException(getHealth());
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	
 	
 	public String getName() {
 		return name;
@@ -38,7 +63,7 @@ class Hero{
 		if (exp >= 0)
 			this.exp = exp;
 		else
-			throw new Exception("经验值不能为负数！");
+			throw new LessThanZeroException((int)exp);
 		
 	}
 	
